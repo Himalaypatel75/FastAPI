68b03081f06b4b68f5c06a8f100fe0e2066be006
himalay patel <himalaypatel75@gmail.com>
2023-09-17 10:59:12 +0530

    initial changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FastAPI
+
+Swagger Doc :- http://127.0.0.1:8000/docs
--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+from typing import Optional
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/{user}')
-def index(user : int):
-    return {"data" : {"name" : user}}
+#path variable Example
+@app.get("/user/{user}")
+async def index(user: int):
+    return {"data": {"name": user}}
+
+#query parameter variable Example
+@app.get("/query")
+async def query_get(limit: int = 10, sort: Optional[str] = None):
+    return {"data": {"limit": limit}}
+
+class Blog(BaseModel):
+    name : str
+    description : Optional[str] = None
+    price : float
+    tax : Optional[int] = None
+
+
+#post methord
+@app.post("/blog")
+async def create_blog(blog: Blog):
+    # print(blog.name)
+    return {"data" : blog}
